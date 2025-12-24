e90ec1c20bbd7ba0b794c2c5d6affa07ca9f711c
Nigel Page <nigelp@synteca.com>
2025-12-25 10:57:30 +1100

    Reconfigure handlers into a list

--- pennant.go
+++ pennant.go
@@ -6,17 +6,28 @@ import (
 	"github.com/nigelpage/pennant/handlers"
 )
 
+type handler struct{
+	urlPattern string
+	function echo.HandlerFunc
+}
+
+var handlersList = []handler{
+	{"/", handlers.WeekendCompetitionHandler},
+	{"/:competition", handlers.CompetitionHandler},
+	{"/authenticate", handlers.AuthenticationHandler},
+	{"/lock", handlers.LockAuthenticationHandler},
+}
+
 func main() {
 	app := echo.New()
 	/* Setup a handler for static files (e.g. CSS, JS etc...) */
 	app.Static("/static", "./assets")
 	
-	/* Setup main handler */
-	app.GET("/", handlers.WeekendCompetitionHandler)
-	app.GET("/:competition", handlers.CompetitionHandler)
-	app.POST("/authenticate", handlers.AuthenticationHandler)
-	app.GET("/lock", handlers.LockAuthenticationHandler)
+	/* Register handlers */
+	for _, h := range handlersList {
+		app.GET(h.urlPattern, h.function)
+	}
 
 	/* Start HTTP server */
-	app.Logger.Fatal(app.StartTLS(":4000", "cert.pem", "key.pem"))
+	app.Logger.Fatal(app.Start(":4000"))
 }
