1d0461942a4674bf1b18e6ec3fbaca65fd5188a4
Nigel Page <nigelp@synteca.com>
2026-02-11 17:47:38 +1100

    Support return of proper HTTP status codes and messages

--- pages/pennant/handlers/pennantHandler.go
+++ pages/pennant/handlers/pennantHandler.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/labstack/echo/v4"
 
@@ -16,13 +17,13 @@ import (
 	"github.com/nigelpage/hbc/pages/pennant/templates"
 )
 
-func getStoredMatches(comp string) js.MatchStore {
+func getStoredMatches(comp string, we time.Time) (js.MatchStore, error) {
 	// Placeholder function to simulate fetching stored matches
-	pennantCompetitionStore := fmt.Sprintf("./common/store/json/%s20251108.json", string(comp[0]))
+	pennantCompetitionStore := fmt.Sprintf("./common/store/json/%s%s.json", string(comp[0]), we.Format("20060102"))
 	jsonFile, err := os.Open(pennantCompetitionStore)
 	if err != nil {
-		fmt.Println("Error opening JSON file:", err)
-		return js.MatchStore{}
+		return js.MatchStore{},
+			   fmt.Errorf("No data found for %s competition, week ending %s: %w", comp, we.Format("02-Jan-2006"), err)
 	}
 	defer jsonFile.Close()
 
@@ -31,19 +32,39 @@ func getStoredMatches(comp string) js.MatchStore {
 	var matchStore js.MatchStore
 	json.Unmarshal(byteValue, &matchStore)
 
-	return matchStore
+	return matchStore, nil
 }
 
 /* Main Pennant page handler */
 
 func PennantHandler(ctx echo.Context) error {
 	comp := ctx.Param("competition")
-	if (comp == "") {
-		comp = "weekend"
-	} else {
+	if (comp != "") {
 		comp = strings.ToLower(comp)
+	} else {
+		comp = "weekend"
 	}
-	store := getStoredMatches(comp)
+
+	layout := "02-Jan-2006"
+	we := time.Now()
+	we = we.AddDate(0, 0, -int(we.Weekday())+6) // Get the upcoming Saturday
+
+	weekEnding := ctx.QueryParam("weekEnding")
+
+	var err error
+	
+	if (weekEnding != "") {
+		we, err = time.Parse(layout, weekEnding)
+		if (err != nil) {
+			return echo.NewHTTPError(http.StatusBadRequest,
+									 fmt.Sprintf("Invalid weekEnding format: %s - expected format is DD-MMM-YYYY", weekEnding))
+		}
+	}
+
+	store, err := getStoredMatches(comp, we)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
 
 	return common.TemplateRenderer(ctx, http.StatusOK, ht.CreatePageFromTemplate(ctx, templates.PennantLayout(store, templates.Icons)))
 }
